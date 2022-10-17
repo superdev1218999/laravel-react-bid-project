06c475d83062c76bf1de6b0b87b9e37d1ce57946
petartrajkovic <petartrajkovic777@gmail.com>
2022-10-17 22:47:16 +0200

    made model migration seed structure

--- database/migrations/2022_10_17_204600_create_bids_table.php
+++ database/migrations/2022_10_17_204600_create_bids_table.php
@@ -14,20 +14,7 @@ class CreateBidsTable extends Migration
     public function up()
     {
         Schema::create('bids', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('ref_id')->unique();
-            $table->string('title');
-            $table->bigInteger('cost');
-            $table->bigInteger('price');
-            $table->string('config');
-            $table
-                ->bigInteger('actual_cost')
-                ->nullable()
-                ->default(null);
-            $table
-                ->bigInteger('actual_cost_confidence_factor')
-                ->nullable()
-                ->default(null);
+            $table->id();
             $table->timestamps();
         });
     }
